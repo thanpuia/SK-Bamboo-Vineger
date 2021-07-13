a7ca2472c6701dfcc32f5f8f14d7d12df861fecf
DESKTOP-F5JF0TT\MSeGS <thanpuia46@gmail.com>
2021-07-14 00:13:35 +0530

    dynamic edit

---
+++ js/functions.js
@@ -0,0 +1,63 @@
+const brandIdentity = document.getElementById("brandIdentity");
+const brandIdentityButton = document.getElementById("brandIdentityButton");
+
+const goals = document.getElementById("goals");
+const goalsButton = document.getElementById("goalsButton");
+
+const vision = document.getElementById("vision");
+const visionButton = document.getElementById("visionButton");
+
+const work = document.getElementById("work");
+const workButton = document.getElementById("workButton");
+
+brandIdentityButton.addEventListener('click',(e) => {
+    e.preventDefault();
+    const newData ={
+        body:brandIdentity.value    
+    };
+    const updates={};
+    updates['/content/index/brandIdentity'] = newData;
+    database.ref().update(updates);
+});
+
+
+goalsButton.addEventListener('click',(e) => {
+    e.preventDefault();
+    const newData ={
+        body:goals.value    
+    };
+    const updates={};
+    updates['/content/index/goals'] = newData;
+    database.ref().update(updates);
+});
+
+visionButton.addEventListener('click',(e) => {
+    e.preventDefault();
+    const newData ={
+        body:vision.value    
+    };
+    const updates={};
+    updates['/content/index/vision'] = newData;
+    database.ref().update(updates);
+});
+
+
+workButton.addEventListener('click',(e) => {
+    e.preventDefault();
+    const newData ={
+        body:work.value    
+    };
+    const updates={};
+    updates['/content/index/work'] = newData;
+    database.ref().update(updates);
+});
+//add new
+//addButton.addEventListener('click',(e) => {
+//  e.preventDefault();
+//  database.ref('content/somevalue').set({
+//      key:value,
+//      key:value    
+//  });
+//});
+
+
